a97b0d3bdd935dcee1f9f1e4a13a302fb6d385d2
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-08-10 16:32:04 -0300

    chore: personal expenditures flutter

---
+++ README.md
@@ -0,0 +1,7 @@
+### Projects of this repository
+
+- personal-expenditures-flutter
+- winforms-app
+- electron-app
+- electron-markdown-editor
+- survey-flutter-project
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'components/my_home_page.dart';
+
+void main(List<String> args) {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
